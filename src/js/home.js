f6dd9ff4b4410327537e0d0b88c4186d35f6fc0d
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2019-10-23 18:00:31 -0400

    Promesas

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,37 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUser1 = new Promise(function(todoBien, todoMal) {
+  // llama un api
+  setTimeout(function(){
+    // Ejecuta cuando pasa el tiempo
+    todoBien('funcion todoMal es la misma funcion anonima que esta de parametro del .catch');
+  }, 5000)
+});
+const getUser2 = new Promise(function(todoBien, todoMal) {
+  // llama un api
+  setTimeout(function(){
+    // Ejecuta cuando pasa el tiempo
+    todoBien('2funcion todoMal es la misma funcion anonima que esta de parametro del .catch');
+  }, 3000)
+});
+
+// getUser
+//   .then(function(){
+//     console.log('Todo esta bien en la vida');
+//   })
+//   .catch(function(message){
+//     console.log(message);
+//   })
+
+Promise.race([
+  getUser1,
+  getUser2,
+])
+.then((message) => {
+  console.log(message);
+})
+.catch((message) => {
+  console.log(message);
+});
