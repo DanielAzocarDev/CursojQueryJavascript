5a60818e85999371ea2bb6e1b42152f7ee27ec5e
Daniel Azocar <danielazocarvelasquezdev@gmail.com>
2019-11-05 21:33:05 -0400

    half of dataset

--- src/js/home.js
+++ src/js/home.js
@@ -85,10 +85,10 @@ Promise.race([
   const animationList = await getData(`${API}list_movies.json?genre=animation`);
   console.log(actionList, dramaList, animationList);
 
-  const videoItemTemplate = (movie) => {
+  const videoItemTemplate = (movie, category) => {
     return(
       `
-      <div class="primaryPlaylistItem">
+      <div class="primaryPlaylistItem" data-id="${movie.id}" data-category="${category}">
         <div class="primaryPlaylistItem-image">
           <img src="${movie.medium_cover_image}">
         </div>
@@ -108,7 +108,8 @@ Promise.race([
 
   const addEventClick = (el)  => {
     el.addEventListener('click', () => {
-      alert('click');
+      // alert('click');
+      showModal(el);
     });
   };
 
@@ -116,7 +117,7 @@ Promise.race([
     // actionList.data.movies es el parametro liste que se le pasa a la funcion
     $container.children[0].remove();
     list.forEach((movie) => {
-      const HTMLToString = videoItemTemplate(movie);
+      const HTMLToString = videoItemTemplate(movie, category);
       const movieElement = createTemplate(HTMLToString);
       $container.append(movieElement);
 
@@ -124,23 +125,60 @@ Promise.race([
     });
   };
 
+  const $home = document.getElementById('home');
+
   const $form = document.getElementById('form');
-  $form.addEventListener('submit', (ev) => {
+
+  const setAttributes = ($element, attributes) => {
+    for(const attribute in attributes){
+      $element.setAttribute(attribute, attributes[attribute]);
+    }
+  };
+  
+  const $featuringContainer = document.getElementById('featuring');
+  const featuringTemplate = (peli) => {
+    return (
+      `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+      `
+    )
+  };
+  $form.addEventListener('submit', async (ev) => {
     ev.preventDefault();
+    $home.classList.add('search-active');
+    const $loader = document.createElement('img');
+    setAttributes($loader,{
+      src: 'src/images/loader.gif',
+      height: 50,
+      width: 50,
+    });
+    $featuringContainer.append($loader);
+
+    const data = new FormData($form);
+    const {data: {movies: pelis}} = await getData(`${API}list_movies.json?limite=1&query_term=${data.get('name')}`)
+
+    const HTMLString = featuringTemplate(pelis[0]);
+    $featuringContainer.innerHTML = HTMLString;
     
   });
   
   const $actionContainer = document.getElementById('action');
-  renderMovieList(actionList.data.movies, $actionContainer);
+  renderMovieList(actionList.data.movies, $actionContainer, 'action');
 
   const $dramaContainer = document.getElementById('drama');
-  renderMovieList(dramaList.data.movies, $dramaContainer);
+  renderMovieList(dramaList.data.movies, $dramaContainer, 'drama');
 
   const $animationContainer = document.getElementById('animation');
-  renderMovieList(animationList.data.movies, $animationContainer);
+  renderMovieList(animationList.data.movies, $animationContainer, 'animation');
   
-  const $featuringContainer = document.getElementById('featuring');
-  const $home = document.getElementById('home');
 
   const $modal = document.getElementById('modal');
   const $overlay = document.getElementById('overlay');
@@ -149,5 +187,20 @@ Promise.race([
   const $modalTitle = $modal.querySelector('h1');
   const $modalImage = $modal.querySelector('img');
   const $modalIDescription = $modal.querySelector('p');
+
+  const showModal = (el) => {
+    $overlay.classList.add('active');
+    $modal.style.animation ='modalIn .8s forwards';
+
+  }
+
+  const hideModal = () => {
+    $modal.style.animation ='modalOut .6s forwards';
+    setTimeout(() => {
+      $overlay.classList.remove('active');
+    }, 700);
+  };
+
+  $hideModal.addEventListener('click', hideModal);
 
 })();
